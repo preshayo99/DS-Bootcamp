ffa04e6e36aa1aa8e01cd8f9c1812f428b15cd65
Precious Inegbenose <78587294+preshayo99@users.noreply.github.com>
2024-08-24 01:52:42 +0100

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,22 @@
+# Declare the variables with provided values
+# Convert variables to different data types
+# Convert num1 to an integer using int()
+# Convert num2 to a float using float()
+# Convert num3 to a string using str()
+# Convert string1 to an integer using int()
+# Print out the converted variables on separate lines
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+print(num1)
+print(num2)
+print(num3)
+print(string1)
